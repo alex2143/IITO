2d3e9eacf3a377653ba236b1c9aef38d50689aee
Alexander Bakker <alex2143@gmail.com>
2015-03-23 16:55:28 +0100

    Menu's met active werken nu ook. Zijn wel lelijk gehardcode maar boeiend

--- header.php
+++ header.php
@@ -32,11 +32,16 @@ else{
 
 			<div class="custom-links">
 				<ul>
-					<li class="active"><a href="index.php">Web applications</a></li>
-					<li class="active"><a href="1.php">Utilization</a></li>
-					<li class="active"><a href="3v3.php">Calamities</a></li>
-					<li class="active"><a href="4.php">WRB Extension</a></li>
-					<li class="active"><a href="5.php">Costs &amp; Savings</a></li>
+					<li class="<?php $link =  "//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+							$escaped_link = htmlspecialchars($link, ENT_QUOTES, 'UTF-8');; echo (strpos($escaped_link,"index.php") || $escaped_link == "http://iito.nl/") > 0 ? "active" : ""; ?>"><a href="index.php">Web applications</a></li>
+					<li class="<?php $link =  "//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+							$escaped_link = htmlspecialchars($link, ENT_QUOTES, 'UTF-8');; echo strpos($escaped_link,"1.php") > 0 ? "active" : ""; ?>"><a href="1.php">Utilization</a></li>
+					<li class="<?php $link =  "//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+							$escaped_link = htmlspecialchars($link, ENT_QUOTES, 'UTF-8');; echo strpos($escaped_link,"3v3.php") > 0 ? "active" : ""; ?>"><a href="3v3.php">Calamities</a></li>
+					<li class="<?php $link =  "//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+							$escaped_link = htmlspecialchars($link, ENT_QUOTES, 'UTF-8');; echo strpos($escaped_link,"4.php") > 0 ? "active" : ""; ?>"><a href="4.php">WRB Extension</a></li>
+					<li class="<?php $link =  "//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+							$escaped_link = htmlspecialchars($link, ENT_QUOTES, 'UTF-8');; echo strpos($escaped_link,"5.php") > 0 ? "active" : ""; ?>"><a href="5.php">Costs &amp; Savings</a></li>
 
 
 					
